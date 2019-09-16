048191c10c32fae15c142735edf4dbfb2d17d571
wades1248 <wades1248@gmail.com>
2019-09-16 17:47:54 -0500

    fixed spelling errors

--- src/bio.js
+++ src/bio.js
@@ -9,7 +9,7 @@ class Bio extends Component{
                 <div className='.image'>
                         <img alt='cartoon protrait of Spencer' src='https://github.com/wades1248/newProtfolio/blob/master/icon.jpeg?raw=true'/>
                     </div>
-                    <p>A future graduate from Thinkful’s Engineering Flex program, I like to with things with wood,
+                    <p>A future graduate from Thinkful’s Engineering Flex program, I like to make things with wood,
                     graphite on paper, and now with code and script. For me, building an app or site follows much the same
                     process as creating a drawing or painting: start with an idea, put an outline on paper, flush out the
                     details, and work until it’s perfect. In my free time I enjoy board games and 
