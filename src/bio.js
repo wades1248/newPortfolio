81e55f68cef1dc30e2af1d1aed7f6d65b3a7b6f4
wades1248 <wades1248@gmail.com>
2019-09-16 12:18:46 -0500

    favicon added

--- src/bio.js
+++ src/bio.js
@@ -5,14 +5,16 @@ class Bio extends Component{
         return(
             <div className='bio' id='bio'>
                 <h1>About Me:</h1>
-                <p>A future graduate from Thinkful’s Engineering Flex program, I like to with things with wood,
+                <div className="bioInfo">
+                <div className='.image'>
+                        <img alt='cartoon protrait of Spencer' src='https://github.com/wades1248/newProtfolio/blob/master/icon.jpeg?raw=true'/>
+                    </div>
+                    <p>A future graduate from Thinkful’s Engineering Flex program, I like to with things with wood,
                     graphite on paper, and now with code and script. For me, building an app or site follows much the same
                     process as creating a drawing or painting: start with an idea, put an outline on paper, flush out the
                     details, and work until it’s perfect. In my free time I enjoy board games and 
                     table tops, hiking, and swimming.
-                </p>
-                <div className='.image'>
-                    <img alt='cartoon protrait of Spencer' src='icon.jpeg'/>
+                    </p>
                 </div>
             </div>
         )
