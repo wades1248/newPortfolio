d750bebd76484db72405738902b6b49df0f9e747
wades1248 <wades1248@gmail.com>
2019-10-05 14:24:21 -0500

    fixed spelling error

--- src/projectStore.js
+++ src/projectStore.js
@@ -67,7 +67,7 @@ const projects = [
                 'https://github.com/wades1248/Found/blob/master/Screen%20Shot%202019-10-02%20at%209.44.37%20PM.png?raw=true'
             ]
         },
-        description: 'Found is a lost and found app for businesses. Businesses can post items that have been left behind by patrons and patrons can search for lost items by city, business, type, and description. Confirmation numbers are used to seacrch for speciffic posts and verify authorization to delete posts.',
+        description: 'Found is a lost and found app for businesses. Businesses can post items that have been left behind by patrons and patrons can search for lost items by city, business, type, and description. Confirmation numbers are used to search for speciffic posts and verify authorization to delete posts.',
         tech: 'HTML5, CSS, Javascript, REACT, Nodes.js, PostgreSQL',
         links: {
             live:'https://lost-found-found-found.now.sh/',
