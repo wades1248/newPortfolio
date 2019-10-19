36a30976b138a79e80cb2149df241ba1d5e26d6a
wades1248 <wades1248@gmail.com>
2019-10-18 22:42:53 -0500

    new backgorund

--- src/bio.js
+++ src/bio.js
@@ -6,8 +6,8 @@ class Bio extends Component{
             <div className='bio' id='bio'>
                 <h1>About Me:</h1>
                 <div className="bioInfo">
-                <div className='.image'>
-                        <img alt='cartoon protrait of Spencer' src='https://github.com/wades1248/newProtfolio/blob/master/icon.jpeg?raw=true'/>
+                    <div className='.image'>
+                            <img alt='cartoon protrait of Spencer' src='https://github.com/wades1248/newProtfolio/blob/master/icon.jpeg?raw=true'/>
                     </div>
                     <p>A future graduate from Thinkful’s Engineering Flex program, I like to make things with wood,
                     graphite on paper, and now with code and script. For me, building an app or site follows much the same
--- src/titlePage.js
+++ src/titlePage.js
@@ -3,12 +3,14 @@ import React, {Component} from 'react';
 class TitlePage extends Component{
     render(){
         return(
-        <div className='title'>
-            <span></span>
-            <h1 className="hero">spencer wade</h1>
-            <div className="heading">
-                <h2>artist</h2>
-                <h2>developer</h2>
+        <div className='titleBox'>
+            <div className='title'>
+                <span></span>
+                <h1 className="hero">spencer wade</h1>
+                <div className="heading">
+                    <h2>artist</h2>
+                    <h2>developer</h2>
+                </div>
             </div>
         </div>
         )
